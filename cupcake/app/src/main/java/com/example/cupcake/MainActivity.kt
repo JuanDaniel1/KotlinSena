c404ed1e65b85d824f26266998754bab9d4e64bb
JuanDaniel1 <juandabogo@gmail.com>
2023-08-01 01:06:16 -0500

    fine

---
+++ cupcake/app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.cupcake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.cupcake.ui.theme.CupcakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CupcakeTheme {
+                // A surface container using the 'background' color from the theme
+
+                    CupcakeApp()
+
+            }
+        }
+    }
+}
+
